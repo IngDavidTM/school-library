65254dcf66a51f5cc17863e65b004daef6ae8f8c
IngDavidTM <david-1011@hotmail.com>
2022-12-19 18:53:18 -0500

    add recommendations

--- person.rb
+++ person.rb
@@ -9,20 +9,18 @@ class Person
     @parent_permission = parent_permission
   end
 
+  def can_use_services?
+    of_age? || @parent_permission
+  end
+
   private
 
   def generate_id
-    1
+    @id = Random.rand(1..1000)
   end
 
   def of_age?
     @age >= 18
-  end
-
-  public
-
-  def can_use_services?
-    of_age? || @parent_permission
   end
 end
 
