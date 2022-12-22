94896df8ccaecb9ce47668ec56c1eb72790bece1
IngDavidTM <david-1011@hotmail.com>
2022-12-21 21:20:28 -0500

    Add person create and list

--- app.rb
+++ app.rb
@@ -1,3 +1,6 @@
+require_relative 'student'
+require_relative 'teacher'
+
 ACTIONS = {
   1 => :list_books,
   2 => :list_people,
@@ -10,13 +13,13 @@ ACTIONS = {
 
 class App
   def initialize
+    puts 'Welcome to School Library App!'
     @books = []
     @people = []
     @rentals = []
   end
 
   def options
-    puts 'Welcome to School Library App!'
     puts ''
     puts 'Please choose an option by enterin a number:'
     puts '1 - List all books'
@@ -49,7 +52,9 @@ class App
   end
 
   def list_people
-    puts 'list people'
+    @people.each do |object|
+      puts "[#{object.class}] Name: #{object.name}, ID: #{object.id}, Age: #{object.age}"
+    end
   end
 
   def create_person
@@ -57,9 +62,27 @@ class App
     option_person = gets.chomp.to_i
     case option_person
     when 1
-      puts option_person
+      print 'Age: '
+      person_age = gets.chomp.to_i
+      print 'Name: '
+      person_name = gets.chomp.to_s
+      print 'Has parent permission? [Y/N]: '
+      person_permission = gets.chomp.to_s
+      if person_permission.capitalize == 'N'
+        @people.push(Student.new(person_age, name: person_name, parent_permission: false))
+      else
+        @people.push(Student.new(person_age, name: person_name))
+      end
+      puts 'Person created successfully'
     when 2
-      puts 'a'
+      print 'Age: '
+      person_age = gets.chomp.to_i
+      print 'Name: '
+      person_name = gets.chomp.to_s
+      print 'Specialization: '
+      person_specialization = gets.chomp.to_s
+      @people.push(Teacher.new(person_age, person_specialization, name: person_name))
+      puts 'Person created successfully'
     else
       puts 'Error: Invalid number, try again'
       send(:create_person)
--- teacher.rb
+++ teacher.rb
@@ -12,9 +12,3 @@ class Teacher < Person
     true
   end
 end
-
-teacher1 = Teacher.new(31, 'Manager', name: 'Paco')
-puts teacher1.name
-puts teacher1.age
-puts teacher1.specialization
-puts teacher1.can_use_services?
