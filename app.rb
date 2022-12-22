8e8c5edc28f140cc7296ebca617413c33f0e41f9
IngDavidTM <david-1011@hotmail.com>
2022-12-21 22:17:08 -0500

    Add create and list rentals by id

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require_relative 'student'
 require_relative 'teacher'
 require_relative 'book'
+require_relative 'rental'
 
 ACTIONS = {
   1 => :list_books,
@@ -99,6 +100,32 @@ class App
     book_author = gets.chomp.to_s
     @books.push(Book.new(book_title, book_author))
     puts 'Book created successfully'
+  end
+
+  def create_rental
+    puts 'Select a book from the following list by number'
+    @books.each_with_index do |object, index|
+      puts "#{index}) Title: '#{object.title}', Author: #{object.author}"
+    end
+    book_number = gets.chomp.to_i
+    puts 'Select a person from the following list by number (not id)'
+    @people.each_with_index do |object, index|
+      puts "#{index}) [#{object.class}] Name: #{object.name}, ID: #{object.id}, Age: #{object.age}"
+    end
+    person_number = gets.chomp.to_i
+    print 'Date: '
+    date = gets.chomp.to_s
+    @rentals.push(Rental.new(@books[book_number], @people[person_number], date))
+    puts 'Rental created successfully'
+  end
+
+  def list_rentals
+    print 'ID of person: '
+    person_id = gets.chomp.to_i
+    puts 'Rentals:'
+    @rentals.each do |object|
+      puts "Date: #{object.date}, Book '#{object.book.title}' by #{object.book.author}" if object.person.id == person_id
+    end
   end
 end
 
